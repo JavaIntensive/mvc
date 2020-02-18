dc2a493a5d716ba545ea31debef105010338366c
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-18 16:56:11 -0500

    First

---
+++ src/main/java/com/example/group3/Group3Application.java
@@ -0,0 +1,13 @@
+package com.example.group3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+
+@SpringBootApplication
+public class Group3Application {
+ public static void main(String[] args) {
+  SpringApplication.run(Group3Application.class, args);
+ }
+
+}
